706f1d6fed56a2374e9cc86a389759230c74d19d
Hwamok Lee <ghkahr1890@gmail.com>
2025-04-17 16:28:35 +0900

    Initial commit

---
+++ example/image_search_app_example.dart
@@ -0,0 +1,6 @@
+import 'package:image_search_app/image_search_app.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/image_search_app_test.dart
@@ -0,0 +1,16 @@
+import 'package:image_search_app/image_search_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
